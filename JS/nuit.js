16731afcfd3cbb4d0e933b12f6801a311781c967
Loick950 <loick.lp59@gmail.com>
2023-03-23 22:37:45 +0100

    mode nuit dans header/footer template a copier coller

---
+++ JS/nuit.js
@@ -0,0 +1,14 @@
+function toggleNightMode() {
+    var body = document.querySelector('body');
+    var modeToggle = document.querySelector('#modeToggle');
+    
+    body.classList.toggle('night-mode');
+    
+    if (body.classList.contains('night-mode')) {
+        modeToggle.src = "../img/light.png";
+        modeToggle.alt = "Désactiver le mode nuit";
+    } else {
+        modeToggle.src = "../img/ph_moon-thin.png";
+        modeToggle.alt = "Activer le mode nuit";
+    }
+}
